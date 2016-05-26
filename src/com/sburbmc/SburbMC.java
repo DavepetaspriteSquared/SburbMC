d192c7b8ce982854b870c6777748a9fe944c321b
Davepetasprite^2 <modulations413@gmail.com>
2016-05-26 13:52:28 -0400

    Added Even More Things
    Who knows if Derspit will be ready soon?
    
    Not me fo- Oh wait yea I do

--- src/com/sburbmc/SburbMC.java
+++ src/com/sburbmc/SburbMC.java
@@ -15,9 +15,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sburbmc.quirks.Quirk;
 
+@SuppressWarnings("unused")
 public class SburbMC extends JavaPlugin{
 
 	ArrayList<Quirk> quirks;
+	
+	ArrayList<Player> Asleep = new ArrayList<Player>();
+	ArrayList<Player> Awake = new ArrayList<Player>();
+	
+	ArrayList<Player> Prospit = new ArrayList<Player>();
+	ArrayList<Player> Derse = new ArrayList<Player>();
 	
 	public void onEnable() {
 		
@@ -44,32 +51,17 @@ public class SburbMC extends JavaPlugin{
 		
 	}
 	
-//	public String getChannel( ArrayList<Quirk> q, String uuid ) {
-//		p.sendMessage("getPlayerQuirk");
-//		for (Quirk a : q) { // Every quirk
-//			if (a.containsPlayer(uuid)){
-//				return a.getName();
-//			}
-//		}
-//	
-//		return " ";
-//	}
-	
 	Player p;
 	
-//	public void mainChat(AsyncPlayerChatEvent e){	
-//		p.setCancelled(true);
-//		  p.setMessage("[" + p.getDisplayName() + "]" + e.getMessage());
-//	}
-	
-	@EventHandler
+/*	@EventHandler
     public void onClick(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK ||  event.getAction() == Action.RIGHT_CLICK_BLOCK) {
         //Teleport Player to Prospit/Derse
         	player.teleport(Bukkit.getWorld("Derspit").getSpawnLocation());
         }
-}
+}*/
+// Will use above code later ^
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -82,9 +74,7 @@ public class SburbMC extends JavaPlugin{
 			p.sendMessage("Args[1]: " + args[1]);
 			
 			if (args.length == 0){
-				p.sendMessage(ChatColor.RED + "Please select a " + ChatColor.DARK_RED + "T" + ChatColor.GOLD + "h"
-						+ ChatColor.GREEN + "i" + ChatColor.DARK_BLUE + "n" + ChatColor.BLUE + "g"
-						+ ChatColor.LIGHT_PURPLE + "!");
+				p.sendMessage(ChatColor.RED + "Please select a Character!");
 			}
 			
 			if (args[0].equalsIgnoreCase("set") && !(args[1].length() > 0)){
@@ -94,19 +84,12 @@ public class SburbMC extends JavaPlugin{
 					if(q.getName().equalsIgnoreCase(args[1])) {
 						q.addPlayer(p.getUniqueId().toString());
 						p.sendMessage("You have been added to: " + q.getName());
-//						if(args[1].equalsIgnoreCase("Porrim")) {
-//							p.setDisplayName(ChatColor.DARK_GREEN + "Porrim");
-//						}if (args[1].equalsIgnoreCase("clear")) {
-//							p.setDisplayName(p.getName() + "");
 						}
 					}
 				}
 		}
 		return false;
 			}
-//		}
-//		return false;
-//	}
 
 	public String getPlayerQuirk( ArrayList<Quirk> q, String uuid ) {
 		p.sendMessage("getPlayerQuirk");
@@ -141,40 +124,40 @@ public class SburbMC extends JavaPlugin{
 		}
 			
 	
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Mituna
-//			String message = e.getMessage().toUpperCase().replace("A", "4").replace("B", "8")
-//					.replace("E", "3").replace("I", "1").replace("O", "0")
-//					.replace("S", "5").replace("T", "7");
-//			e.setMessage(p.getDisplayName() + ChatColor.YELLOW + message);
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Kurloz
-//			e.setMessage(p.getDisplayName() + ChatColor.DARK_PURPLE + "...");
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Sollux
-//			String message = e.getMessage().toLowerCase().replace("s", "2")
-//					.replaceAll("i+", "ii").replaceAll("to+", "two");
-//			e.setMessage(p.getDisplayName() + ChatColor.YELLOW + message);
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Vriska
-//			String message = e.getMessage().replaceAll(":*([;:])+([dDbBpPL\\Q)(][\\E])", ":::$1$2")
-//					.replaceAll("([\\.!?])+", "$1$1$1$1$1$1$1$1").replaceAll("[bB]", "8")
-//					.replaceAll("[aA]([iI][tT]|[tT][eE])", "8")
-//					.replaceAll("[aA][tT]([iI][nN][gG])\\b", "8$1");
-//			e.setMessage(p.getDisplayName() + ChatColor.BLUE + message);
-//			
-//			
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Jasprosesprite^2
-//			String message = e.getMessage();
-//			e.setMessage(ChatColor.LIGHT_PURPLE + "Jasprosesprite^2: " + ChatColor.DARK_PURPLE + message);
-//			
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Davepetasprite^2
-//			String message = e.getMessage().toLowerCase().replaceAll("[e]{2},", "33")
-//					.replace("ver", "fur").replace("pos", "paws").replaceAll("([^\\.])\\.{1,2}([^\\.])", "$1$2")
-//					.replaceAll("\\.+", "...").replaceAll("[\\W&&[^\\s\\.!\\?]]", "");
-//			e.setMessage(ChatColor.GREEN + "Davepetasprite^2: B33 < " + ChatColor.GOLD + message);
-//			
-//		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Erisolsprite
-//			String message = e.getMessage().toLowerCase().replace("s", "2")
-//					.replaceAll("i+", "ii").replaceAll("to+", "two").replaceAll("([vwVW])", "$1$1").replaceAll("\\ban\\b", "a")
-//					.replaceAll("\\band\\b", "an").replaceAll("\\b(.*in)g\\b", "$1");
-//			e.setMessage(ChatColor.GREEN + "Erisolsprite: " + ChatColor.GREEN + message);
-//		}
+/*		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Mituna
+			String message = e.getMessage().toUpperCase().replace("A", "4").replace("B", "8")
+					.replace("E", "3").replace("I", "1").replace("O", "0")
+					.replace("S", "5").replace("T", "7");
+			e.setMessage(p.getDisplayName() + ChatColor.YELLOW + message);
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Kurloz
+			e.setMessage(p.getDisplayName() + ChatColor.DARK_PURPLE + "...");
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Sollux
+			String message = e.getMessage().toLowerCase().replace("s", "2")
+					.replaceAll("i+", "ii").replaceAll("to+", "two");
+			e.setMessage(p.getDisplayName() + ChatColor.YELLOW + message);
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Vriska
+			String message = e.getMessage().replaceAll(":*([;:])+([dDbBpPL\\Q)(][\\E])", ":::$1$2")
+					.replaceAll("([\\.!?])+", "$1$1$1$1$1$1$1$1").replaceAll("[bB]", "8")
+					.replaceAll("[aA]([iI][tT]|[tT][eE])", "8")
+					.replaceAll("[aA][tT]([iI][nN][gG])\\b", "8$1");
+			e.setMessage(p.getDisplayName() + ChatColor.BLUE + message);
+			
+			
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Jasprosesprite^2
+			String message = e.getMessage();
+			e.setMessage(ChatColor.LIGHT_PURPLE + "Jasprosesprite^2: " + ChatColor.DARK_PURPLE + message);
+			
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Davepetasprite^2
+			String message = e.getMessage().toLowerCase().replaceAll("[e]{2},", "33")
+					.replace("ver", "fur").replace("pos", "paws").replaceAll("([^\\.])\\.{1,2}([^\\.])", "$1$2")
+					.replaceAll("\\.+", "...").replaceAll("[\\W&&[^\\s\\.!\\?]]", "");
+			e.setMessage(ChatColor.GREEN + "Davepetasprite^2: B33 < " + ChatColor.GOLD + message);
+			
+		} else if (players.contains(p) && players.contains(p.getUniqueId().toString())) { //Erisolsprite
+			String message = e.getMessage().toLowerCase().replace("s", "2")
+					.replaceAll("i+", "ii").replaceAll("to+", "two").replaceAll("([vwVW])", "$1$1").replaceAll("\\ban\\b", "a")
+					.replaceAll("\\band\\b", "an").replaceAll("\\b(.*in)g\\b", "$1");
+			e.setMessage(ChatColor.GREEN + "Erisolsprite: " + ChatColor.GREEN + message);
+		}*/
 	}
 }
